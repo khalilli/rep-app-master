8e8d0e397c6fdb42198af44dda7a0749d45a1aea
luthien017 <esmerkhalilli@gmail.com>
2022-04-06 15:50:43 +0400

    iiii

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -29,10 +29,7 @@ const Homepage = () => {
         "http://192.168.14.33/otcs/llisapi.dll?func=ll&objId=113704&objAction=RunReport&nexturl=%2Fotcs%2Fllisapi%2Edll%3Ffunc%3Dll%26objId%3D113704%26objAction%3DEditView%26viewType%3D1%26nexturl%3D%252Fotcs%252Fllisapi%252Edll%253Ffunc%253Dll%2526objid%253D100991%2526objAction%253Dbrowse%2526sort%253Dname"
       )
       .then((response) => {
-        console.log("id", response.data[0].id);
-        console.log("Date",response.data[0].taskdate);
-        console.log("Start time", response.data[0].start_time);
-        console.log("Response data",response.data);
+        console.log("length",response.data.length);
         for (var i=0; i<response.data.length; i++){
           const Task = {
             date: response.data[i].taskdate,
@@ -71,12 +68,12 @@ const Homepage = () => {
   };
 
     const AddTaskHandler = (enteredTask) => {
-      setLoading(true);
+      // setLoading(true);
       console.log("Entered", enteredTask);
       setTasks((prevTasks)=>{
         return [enteredTask, ...prevTasks];
       });
-      setLoading(false);
+      // setLoading(false);
       {enteredTask.data.map((task) => (
         setData(enteredTask.id, enteredTask.date, task.stime, task.etime, task.tasktitle)
       ))}
