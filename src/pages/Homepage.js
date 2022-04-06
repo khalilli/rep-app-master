b1f0f58c54503fa57a7e15d2488ea97c6fedbe11
luthien017 <esmerkhalilli@gmail.com>
2022-04-06 15:23:15 +0400

    xxx

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -29,9 +29,9 @@ const Homepage = () => {
         "http://192.168.14.33/otcs/llisapi.dll?func=ll&objId=113704&objAction=RunReport&nexturl=%2Fotcs%2Fllisapi%2Edll%3Ffunc%3Dll%26objId%3D113704%26objAction%3DEditView%26viewType%3D1%26nexturl%3D%252Fotcs%252Fllisapi%252Edll%253Ffunc%253Dll%2526objid%253D100991%2526objAction%253Dbrowse%2526sort%253Dname"
       )
       .then((response) => {
-        console.log("id", response.data.id);
-        console.log("Date",response.data.taskdate);
-        console.log("Start time", response.data.start_time);
+        console.log("id", response.data[0].id);
+        console.log("Date",response.data[0].taskdate);
+        console.log("Start time", response.data[0].start_time);
         console.log("Response data",response.data);
         setTasks(response.data);
       });
