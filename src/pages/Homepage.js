bc984c3ff8a6ebdb648e6197acb0f06f2d68c551
luthien017 <esmerkhalilli@gmail.com>
2022-04-07 10:53:58 +0400

    ,,

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -25,7 +25,7 @@ const Homepage = () => {
   const [tasks, setTasks] = useState(Initdata);
   const [loading, setLoading] = useState(false);
 
-  const taskTables = [{}, ];
+  // const taskTables = [{}, ];
   useEffect(() => {
     axios.get(
         "http://192.168.14.33/otcs/llisapi.dll?func=ll&objId=113704&objAction=RunReport&nexturl=%2Fotcs%2Fllisapi%2Edll%3Ffunc%3Dll%26objId%3D113704%26objAction%3DEditView%26viewType%3D1%26nexturl%3D%252Fotcs%252Fllisapi%252Edll%253Ffunc%253Dll%2526objid%253D100991%2526objAction%253Dbrowse%2526sort%253Dname"
@@ -44,7 +44,9 @@ const Homepage = () => {
           };
           console.log("task", Task);
           let tableId = response.data.id;
-          let table = response.data.groupBy(({tableId}) => tableId);
+          const tables=response.data;
+          console.log("tables", tables)
+          const table = tables.groupBy(({tableId}) => tableId);
           console.log("Tables in Homepage", table); 
           setTasks((prevTasks)=>{
             return [Task, ...prevTasks];
