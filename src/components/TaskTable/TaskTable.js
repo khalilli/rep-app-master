3e1589faf5b1f4676ee50c206e12b335ae412e2e
luthien017 <esmerkhalilli@gmail.com>
2022-04-08 17:02:56 +0400

    oo

--- src/components/TaskTable/TaskTable.js
+++ src/components/TaskTable/TaskTable.js
@@ -16,8 +16,8 @@ const TaskTable = (props) => {
 
     const removeTask = (index) => {
         console.log("Index and row and id",index, props.data1[index], props.data1[index].id);
-        row = [...props.data1];
-        setRow(row.splice(index,1));
+        const rows = [...row];
+        setRow(rows.splice(index,1));
 
         if (props.data1.length === 0){
             setShowTable(false);
