d6867576100c6947ad9e9f84646b7ac7723e06b6
luthien017 <esmerkhalilli@gmail.com>
2022-04-11 16:05:59 +0400

    rfv

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -89,11 +89,8 @@ const Homepage = () => {
     axios.get(url);
   }
 
-  const sendWeeklydata = async(taskdate, stime, etime, tasktitle) => {
+  const sendWeeklydata = async(stime, etime, tasktitle) => {
     var url = "http://192.168.14.33/otcs/llisapi.dll?func=ll&objId=115288&objAction=RunReport";
-    if (taskdate){
-      url += `&taskdate=${taskdate}`;
-    }
     if (stime){
       url += `&stime=${stime}`;
     }
@@ -109,28 +106,23 @@ const Homepage = () => {
 
   const sendDate = () => {
     const curr = Date.monday();
-    const taskdates = [];
+    const datas = [];
     tasks.map(task => {
       for (var i=1 ;i<=5; i++){
         const first = curr.getDate() - curr.getDay() + i 
         const day = moment(curr.setDate(first)).format('LL');
         if( task.date === day){
           console.log(task.date);
-          taskdates.push(task.date);
           // sendWeekdate(task.date);
-          // sendWeeklydata(taskdates, task.data[0].stime, task.data[0].etime, task.data[0].tasktitle);
-          console.log(task.date, task.data[0].stime, task.data[0].etime, task.data[0].tasktitle);
-
-          // task.data.map((task) => {
-          //   console.log(task.stime, task.etime, task.tasktitle);
-          //   sendWeeklydata(task.stime, task.etime, task.tasktitle);
-          // });
-
+          task.data.map((task) => {
+            datas.push(task.stime, task.etime, task.tasktitle);
+            // console.log(task.stime, task.etime, task.tasktitle);
+            // sendWeeklydata(task.stime, task.etime, task.tasktitle);
+          });
         console.log("end");
         }
       }
-      console.log(taskdates);
-      sendWeeklydata(taskdates, task.data[0].stime, task.data[0].etime, task.data[0].tasktitle);
+      console.log(datas);
     });
   };
 
