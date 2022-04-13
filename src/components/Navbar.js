60c280cf8fa6fe39c715d96a74a11f6bc6830785
luthien017 <esmerkhalilli@gmail.com>
2022-04-13 16:12:30 +0400

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    navBar: {
+        background: "#041562"
+    },
+  }));
 
 function LinkTab(props) {
   return (
@@ -15,6 +22,8 @@ function LinkTab(props) {
   );
 }
 const Navbar = () => {
+    const classes = useStyles(); 
+
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -23,7 +32,7 @@ const Navbar = () => {
 
     return (
         <Box sx={{ width: '100%' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+        <Tabs value={value} onChange={handleChange} aria-label="nav tabs example" className={classes.navBar}>
             <LinkTab label="HomePage" href="/drafts" />
             <LinkTab label="Another Page" href="/trash" />
         </Tabs>
