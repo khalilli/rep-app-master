a37ec8cd98851dabbad3b1c4583eb698402f0509
luthien017 <esmerkhalilli@gmail.com>
2022-04-13 16:29:17 +0400

    b

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import "./Navbar"
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+    navBar: {
+        background: "#041562"
+    },
+    Tab: {
+        color: "white",
+    }
+  }));
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
 const Navbar = () => {
-return (
-    <ul className="nav justify-content-center">
-        <li className="nav-item">
-            <a className="nav-link" href="#">Homepage</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Another Page</a>
-        </li>
-    </ul>
+    const classes = useStyles(); 
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+    return (
+        <Box sx={{ width: '100%' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="nav tabs example" className={classes.navBar}>
+            <LinkTab label="HomePage" href="/drafts" />
+            <LinkTab label="Another Page" href="/trash" />
+        </Tabs>
+        </Box>
     );
 };
 export default Navbar;
