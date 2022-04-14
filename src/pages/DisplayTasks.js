c0a9f27a98bceafdca557341101bea7580f51166
luthien017 <esmerkhalilli@gmail.com>
2022-04-14 12:22:43 +0400

    ko

--- src/pages/DisplayTasks.js
+++ src/pages/DisplayTasks.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from '@mui/material/TextField';
+import './DisplayTasks';
 import Tasks from '../components/TaskTable/Tasks';
 import './DisplayTasks';
 import Dropdown from './Dropdown';
@@ -7,7 +12,21 @@ import axios from 'axios';
 import moment from 'moment';
 import { v4 as uuid } from 'uuid';
 
+
+const useStyles = makeStyles(theme => ({
+  Ctext: {
+      color: "#051367"
+  },
+  color: {
+    backgroundColor: 'white'
+  },
+  root: {
+    background: "white"
+  }
+}));
+
 const DisplayTasks = (props) => {
+  const classes = useStyles(); 
     const [tasks, setTasks] = useState([]);
 
     const getData = async(userid) => {
@@ -51,10 +70,80 @@ const DisplayTasks = (props) => {
         getData(window.userId);
     }, []);
 
+  
+    const [firstDate, setFirstDate] = useState('');
+    const [secondDate, setSecondDate] = useState('');
+  
+    const firstDateChange = (event) => {
+      setFirstDate(event.target.value);
+    };
+    const secondDateChange = (event) => {
+      setSecondDate(event.target.value);
+    };
+    const submitHandler = (event) => {
+      event.preventDefault();
+      console.log(firstDate)
+      console.log(secondDate)
+  
+      // const weeklytables = [];
+  
+      // const firstDate = Date.monday();
+      // const lastDate = Date.today();
+      // const curr = new Date();
+      // for( var i=0; i<tasks.length; i++){
+      //   for( var j=firstDate.getDate(); j<=lastDate.getDate(); j++){
+      //     const first = j;
+      //     const day = moment(curr.setDate(first)).format('LL');
+      //     if(tasks[i].date === day){
+      //       console.log("Days", tasks[i].date);
+      //       console.log("Tasks", tasks[i]);
+      //       weeklytables.push(tasks[i]);
+      //     }
+      //   }
+      // }
+      // console.log(weeklytables);
+    };
+
     return(
         <div>
           <div>
-            <Dropdown/>
+            <form onSubmit={submitHandler}>
+              <Grid item container direction={"row"} spacing={3} sx={{mt: 3, mb:3}} className={classes.root}>
+                <Grid item>
+                    <TextField
+                    label="From"
+                    id="firstdate"
+                    variant="outlined"
+                    size="small"
+                    type="date"
+                    name="firstdate"
+                    InputLabelProps={{
+                        shrink: true,
+                      }}
+                    value={firstDate} 
+                    onChange={firstDateChange}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                    label="To"
+                    id="seconddate"
+                    variant="outlined"
+                    size="small"
+                    type="date"
+                    name="seconddate"
+                    InputLabelProps={{
+                        shrink: true,
+                      }}
+                    value={secondDate}
+                    onChange={secondDateChange} 
+                    />
+                </Grid>
+                <Grid item>
+                <Button variant="contained" type='submit'>Show</Button>
+                </Grid>
+              </Grid>
+            </form>
           </div>
           <Tasks items={tasks} />
         </div>
