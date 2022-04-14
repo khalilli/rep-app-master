1642bb66dee6760df0cce61f75dcd029567d5048
luthien017 <esmerkhalilli@gmail.com>
2022-04-14 14:39:31 +0400

    m

--- src/routers/AppRoutes.js
+++ src/routers/AppRoutes.js
@@ -1,8 +1,8 @@
 import { useRoutes } from "react-router-dom";
 import { publicRoutes } from "./publicRoutes";
 const AppRoutes = () => {
-    const routes = publicRoutes;
-    const element = useRoutes([...routes]);
+    // const routes = publicRoutes;
+    const element = useRoutes([...publicRoutes]);
     return <>{element}</>
 
 };
