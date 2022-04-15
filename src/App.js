e29eacf88672d8f38f4da868ece73294ea9aa0b0
luthien017 <esmerkhalilli@gmail.com>
2022-04-15 12:02:44 +0400

    z

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Homepage from "./pages/Homepage.js";
 import DisplayTasks from "./pages/DisplayTasks.js";
 import AppProvider from "./providers/AppProvider.js"
 import AppRoutes from "./routers/AppRoutes.js"
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Router>
+        
+      </Router>
       {/* <AppProvider>
         <AppRoutes />
       </AppProvider> */}
-      <Homepage />
+      {/* <Homepage /> */}
     </div>
   );
 }
