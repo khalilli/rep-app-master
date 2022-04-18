04d4b8504781675eb740e90b7b601687e78eae37
luthien017 <esmerkhalilli@gmail.com>
2022-04-18 15:23:17 +0400

    mm

--- src/components/UsersFilter.js
+++ src/components/UsersFilter.js
@@ -3,19 +3,20 @@ const UsersFilter = (props) => {
     const dropdownchange = (event) => {
         props.onChangefilter(event.target.value);
     };
-    console.log(props.items);
+    props.items.map((item) => {return item.userid})
+    console.log("new",props.items);
     return (
       <div className="expenses-filter">
         <div className="expenses-filter__control">
           <label>Choose a user:</label>
           <select value={props.selected} onChange={dropdownchange}>
-            {/* <option value='0'>Users</option>
+            <option value='0'>Users</option>
                 <option value='100991'>100991</option>
                 <option value='18884'>18884</option>
-                <option value='?'>?</option> */}
-            {props.items.map((item) => (
-              new Set(<option value={item.userid}>{item.userid}</option>)
-            ))}
+                <option value='?'>?</option>
+            {/* {props.items.map((item) => (
+              <option value={item.userid}>{item.userid}</option>
+            ))} */}
           </select>
         </div>
       </div>
