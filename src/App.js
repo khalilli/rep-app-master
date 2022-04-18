185ec6a809e48325e0a8614109dd1d2386a0ad66
luthien017 <esmerkhalilli@gmail.com>
2022-04-18 16:06:23 +0400

    ml

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Navbar/>
         <AppRoutes />
       </AppProvider> */}
-      <DisplayTasks />
+      <Homepage />
     </div>
     // <Router>
     //   <div className="App">
