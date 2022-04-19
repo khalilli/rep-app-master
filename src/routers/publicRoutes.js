580e87d711145725c7d45109892ebc60e1c5ce28
luthien017 <esmerkhalilli@gmail.com>
2022-04-19 10:40:01 +0400

    p

--- src/routers/publicRoutes.js
+++ src/routers/publicRoutes.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import Homepage from "../pages/Homepage";
 import DisplayTasks from "../pages/DisplayTasks";
 
 export const publicRoutes = [
-    /*     {
+    {
         path:'/',
         element: (<Navigate to={window.baseUrl + '/homepage/displaytasks' + '?func=ll&objId=' + 107301 + '&objAction=RunReport'} />)
-    }*/
-    {
-        path: window.baseUrl + '?func=ll&objId=' + 107301 + '&objAction=RunReport' + '/',
-        element: <Homepage/>
     },
     {
-        path: window.baseUrl + '?func=ll&objId=' + 107301 + '&objAction=RunReport' + '/displaytasks',
-        element: <DisplayTasks/>
+        path: window.baseUrl,
+        element: (<Navigate to={window.baseUrl + '/homepage/displaytasks' + '?func=ll&objId=' + 107301 + '&objAction=RunReport'} />)
+    },
+    {
+        path: window.baseUrl + '/homepage',
+        element: <Homepage />,
+        children: [
+            {
+                path: 'displaytasks',
+                element: <DisplayTasks/>
+            }
+        ]
     }
+    // {
+    //     path: window.baseUrl + '?func=ll&objId=' + 107301 + '&objAction=RunReport' + '/',
+    //     element: <Homepage/>
+    // },
+    // {
+    //     path: window.baseUrl + '?func=ll&objId=' + 107301 + '&objAction=RunReport' + '/displaytasks',
+    //     element: <DisplayTasks/>
+    // }
 ]
