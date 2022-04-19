f0fdd7033664d84dc16839dd41fe2cee2228f768
luthien017 <esmerkhalilli@gmail.com>
2022-04-19 17:28:25 +0400

    mo

--- src/components/TaskForm/AddForm.js
+++ src/components/TaskForm/AddForm.js
@@ -6,7 +6,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import moment from 'moment';
-import { v4 as uuid } from 'uuid';
 import "./AddForm.css";
 
 const useStyles = makeStyles(theme => ({
@@ -44,9 +43,10 @@ const AddForm = (props) => {
     const submitHandler = (event) => {
         event.preventDefault();
         const taskData = {
-          date: moment(enteredDate).format("LL"),
-          data: { id: uuid(), ...inputFields },
+            date: moment(enteredDate).format('LL'),
+            data: inputFields,
         };
+        // data: { row_id: uuid(), ...inputFields },
         props.onSaveTask(taskData);
     };
 
