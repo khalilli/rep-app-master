ad70dcd093afaacbfb1de62e417cb5ebe5452937
luthien017 <esmerkhalilli@gmail.com>
2022-04-20 14:29:24 +0400

    as

--- src/components/TaskTable/TaskTable.js
+++ src/components/TaskTable/TaskTable.js
@@ -41,6 +41,7 @@ const TaskTable = (props) => {
     };
     return (
       <div>
+        {window.userName}
         {showTable ? (
           <div className="task-table">
             <div className="task-date">{props.date1}</div>
