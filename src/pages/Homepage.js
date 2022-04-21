23b5b9a82b54d3e4668fcbd92ca458cb819f4595
luthien017 <esmerkhalilli@gmail.com>
2022-04-21 12:09:16 +0400

    pl

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -164,6 +164,7 @@ const Homepage = () => {
           {console.log("Actual user id", window.userId)}
           {console.log("Actual group name", window.groupName)}
           {console.log(window.groupId)}
+          {console.log("Group Members", window.groupMembers)}
           <NewTask onAddTask={AddTaskHandler} />
           {/* <Grid container direction="column" alignItems="center">
             <Grid item>
