d16c409a7cc9c15a5607df12b441ab925391095d
luthien017 <esmerkhalilli@gmail.com>
2022-04-22 11:45:52 +0400

    m

--- src/components/TaskTable/TaskTable.js
+++ src/components/TaskTable/TaskTable.js
@@ -40,7 +40,7 @@ const TaskTable = (props) => {
       <div>
         {showTable ? (
           <div className="task-table">
-            <Grid container direction={"row"} spacing={2} sx={{ml: '300px'}}>
+            <Grid container direction={"row"} spacing={2} sx={{ml: '500px'}}>
               <Grid item>
                 <div className="task-date">{props.date1}</div>
               </Grid>
