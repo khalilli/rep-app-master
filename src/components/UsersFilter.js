abf8974d67e0cddcd1e5d410932ab2c222d85509
luthien017 <esmerkhalilli@gmail.com>
2022-04-25 11:33:30 +0400

    zx

--- src/components/UsersFilter.js
+++ src/components/UsersFilter.js
@@ -13,11 +13,11 @@ const UsersFilter = (props) => {
     const uniqueUserids = [...new Set(userids)];
     const uniqueUsernames = [...new Set(usernames)];
     const useroptions = [<option value='0'>All Users</option>];
-    console.log("Unique", uniqueUserids);
+    console.log("Unique id", uniqueUserids);
+    console.log("Unique name", uniqueUsernames);
     for(var i=0;i<uniqueUserids.length;i++){
       useroptions.push(<option value={uniqueUserids[i]}>{uniqueUsernames[i]}</option>)
     }
-    console.log("Useropt", useroptions);
     return (
       <div className="expenses-filter">
         <div className="expenses-filter__control">
