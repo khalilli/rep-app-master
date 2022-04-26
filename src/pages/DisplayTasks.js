608cc4e973c9e697411d84763429ca648f6a9dde
luthien017 <esmerkhalilli@gmail.com>
2022-04-26 10:20:24 +0400

    h

--- src/pages/DisplayTasks.js
+++ src/pages/DisplayTasks.js
@@ -134,7 +134,7 @@ const DisplayTasks = (props) => {
         <div>
           <div>
             <div className='selection'>
-              <UsersFilter selected={filteredUser} onChangefilter={filterChange} items={alltasks}/>
+              <UsersFilter selected={filteredUser} onChangefilter={filterChange} items={alltasks} getTasks={getData}/>
               <form onSubmit={submitHandler}>
               <Grid container direction={"row"} spacing={3} sx={{mt: 3, pb:3, pl:2}} >
                 <Grid item>
